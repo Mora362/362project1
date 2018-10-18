364bd15fa1bffb91ef6eb74a6e36e6d4e608618b
Brigette Mora <37383890+BrigetteMora@users.noreply.github.com>
2018-10-17 23:22:31 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,12 @@
 <script type="text/javascript">
 
+console.log('the button is working');
+
 function submit() {
          alert("form has been submitted!");
 
 }
+
 document.getElementById('submit').addEventListener('click', submit);
 
 </script>
